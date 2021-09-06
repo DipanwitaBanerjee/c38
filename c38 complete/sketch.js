c7c22df60a072fc4f2a841c8991772cacc4c6e47
DipanwitaBanerjee <74285485+DipanwitaBanerjee@users.noreply.github.com>
2021-09-06 13:28:18 +0530

    Add files via upload

--- c38 complete/sketch.js
+++ c38 complete/sketch.js
@@ -8,11 +8,11 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
+//var cars, car1, car2, car3, car4;
 
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+ // canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -21,7 +21,7 @@ function setup(){
 
 
 function draw(){
-  if(playerCount === 2){
+  if(playerCount === 4){
     game.update(1);
   }
   if(gameState === 1){
